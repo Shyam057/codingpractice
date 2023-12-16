88879336c02ce9283471c4f693d3415b11be35d6
Shyam Kumar Bhattarai <shyambhattarai057@gmail.com>
2023-12-16 12:43:31 +0545

    check prime

---
+++ checkPrime.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int flag=0;
+    for(int i=2;i<sqrt(n);i++){
+        if(n%i==0){
+            flag=1;
+            cout<<"Non-prime number";
+            break;
+        }
+    }
+    if(flag==0){
+        cout<<"Prime number";
+    }
+}
